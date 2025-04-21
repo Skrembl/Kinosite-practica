e086b23e9a2cffa1d45548b499bc13ed48a0c952
Ваше Имя <akimowiljagg@outlook.com>
2025-04-21 13:12:28 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,48 @@
-const cards = document.querySelectorAll('.card');
-const background = document.querySelector('.background');
-
-cards.forEach(card => {
-  card.addEventListener('mouseenter', () => {
-    const bg = card.getAttribute('data-bg');
-    background.style.backgroundImage = `url(${bg})`;
-    background.style.opacity = 1;
-  });
-
-  card.addEventListener('mouseleave', () => {
-    background.style.opacity = 0;
-  });
+// ! Обработчик + вывод по айди
+document.querySelectorAll(".card-all").forEach((card) => {
+    card.addEventListener("click", () => {
+        const movieId = card.getAttribute("data-id");
+        const bgImage = card.getAttribute("data-bg");
+        window.location.href = `movies.html?id=${movieId}&bg=${encodeURIComponent(bgImage)}`;
+    });
 });
 
+// ! Вывод нужного блока по айди
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
 
+if (id) {
+    document.querySelectorAll(".movie-card").forEach((card) => {
+        if (card.id === id) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+
+    document.querySelectorAll(".info-movie").forEach((info) => {
+        if (info.id === id) {
+            info.style.display = "block";
+        } else {
+            info.style.display = "none";
+        }
+    });
+}
+
+const usp = new URLSearchParams(window.location.search);
+    const idUsp = usp.get("id");
+    const bgUrl = usp.get("bg");
+
+    if(bgUrl) {
+        const headerBg = document.getElementById("body-bg__movie");
+        headerBg.style.backgroundImage = `url(${decodeURIComponent(bgUrl)})`;
+        headerBg.style.backgroundSize = "cover";
+        headerBg.style.backgroundPosition = "center";
+        headerBg.style.height = "930px";
+    }
+
+    if(id) {
+        document.querySelectorAll(".movie-card").forEach((card) => {
+            card.style.display = card.id === id ? "block" : "none";
+        });
+    }
