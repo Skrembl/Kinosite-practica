b57b7eedae211991d075096bf07b0b1ad6b99828
Ваше Имя <akimowiljagg@outlook.com>
2025-05-02 22:38:38 +0300

    Initial commit

--- server/get_reviews.php
+++ server/get_reviews.php
@@ -1,10 +1,24 @@
 <?php
-$id = $_GET['id'] ?? '0';
-$filename = "../reviews/reviews_" . $id . ".json";
+// $id = $_GET['id'] ?? '0';
+// $filename = "../reviews/reviews_" . $id . ".json";
 
+// if (file_exists($filename)) {
+//     $json = file_get_contents($filename);
+//     echo $json;
+// } else {
+//     echo json_encode([]);
+// }
+$id = $_GET['id'] ?? '0';
+$filename = "../reviews/reviews_" . $id . ".json"; // Путь к файлу
+
+// Проверяем, существует ли файл
 if (file_exists($filename)) {
+    // Читаем содержимое файла
     $json = file_get_contents($filename);
-    echo $json;
+
+    // Выводим содержимое в браузер
+    echo "<h1>Отзывы для фильма с ID: $id</h1>";
+    echo "<pre>" . htmlspecialchars($json) . "</pre>";
 } else {
-    echo json_encode([]); // Возвращаем пустой массив, если отзывов нет
+    echo "<h1>Отзывы не найдены для этого фильма.</h1>";
 }
